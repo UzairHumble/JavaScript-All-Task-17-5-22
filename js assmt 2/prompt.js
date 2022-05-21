209952643867714981d937f426b4e030a90ade42
“Uzair <Uzairmalik2329@gmail.com>
2022-05-21 17:43:57 +0500

    initial

---
+++ js assmt 2/prompt.js
@@ -0,0 +1,20 @@
+// name alert with prompt
+var FullName =prompt("please Enter Your FullName");
+alert("Uzair Malik");
+
+// city name prompt
+var cname = prompt("Enter your city Name");
+alert("Welcome to city of lights");
+
+// gander prpmpt
+var a = prompt("Enter Your Ganger");
+var b = ("male");
+var c = ("female");
+if (b){
+    alert("Good morning Sir");
+}
+  if(c){
+    alert("Good Morning Ma'am");
+}
+
+
---
+++ js asst 1/app.js
@@ -0,0 +1,26 @@
+var userName;
+var myName = "  Uzair Malik";
+var message = "As Salam Waliekum";
+alert(message);
+var firstName = "Uzair Malik";
+alert(firstName);
+var year = "20 years old";
+alert(year);
+var study = "FUUAST";
+alert(study);
+var type = "Mobile & Cloud Computing-Module A";
+alert(type);
+var food = "Biryani";
+alert(food + "\n" + food.substring (0,4) + "\n" + food.substring (0,3)+ "\n" + food.substring (0,2)+ "\n" + food.substring (0,1) );
+var work = "Developer";
+var nameOfWife = "Kyun batao?";
+var kidsNumber = "1 bhi nh he abhi";
+alert("You will be a " + work + " in USA " + " , married with " + nameOfWife + " With " + kidsNumber)
+var email = "abc@xyz.com"
+alert("My email address is " + email)
+var book = "A smarter way to learn Java Script"
+alert("I am trying to learn from the book " + book)
+alert("My name is " + frirstName + ". I am " + year + ". I study in " + study + ". I want to learn " + type + ". I like " + food + ". I am " + work + ". My Wife Name is " + nameOfWife + " , with " + kidsNumber + ". And my email Id is " + email + ".")
+console.log("My name is " + firstName)
+console.log("I study in " + study)
+console.log("I take " + type)
